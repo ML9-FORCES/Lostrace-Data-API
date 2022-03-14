bad89517866401e19bbe0e567733f96d411a2975
Garvit9000c <909garvit@gmail.com>
2022-03-14 12:56:30 +0530

    init

--- app.py
+++ app.py
@@ -22,27 +22,27 @@ app.before_request(before_request)
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    if request.method == 'POST':
-        `if 'img' not in request.files:
-            return 'there is no file1 in form!'
-        File = request.files['img']
-        format = '.'+File.filename.split('.')[-1]
-        temp = tempfile.NamedTemporaryFile(suffix=format)
-        File.save(temp.name)
-        #load(temp.name)
-        temp.close()
-        res = {
-        "Img": "https://trackthemissingchild.gov.in/trackchild/intra_trackchild/images_missing/3281805mpw20220010.jpg",
-        "Name": "PANCHANU KUMAR RAJAK",
-        "Current_Age": "17 Years 0 Months 6 Days",
-        "Gender": "MALE",
-        "Father_Name": "BHUDEB RAJAK",
-        "Place_of_Missing": "SREEBHUMI",
-        "Date_of_Missing": "10/03/2022",
-        "Profile_link": "https://trackthemissingchild.gov.in/trackchild/missing_dtl.php?		missing_id=47.46.52.45.52.44.49.105.108.115.46.44.46.46.44.44.45.44.102#verticalTab1",
-        "Report_link": "https://trackthemissingchild.gov.in/trackchild/photograph_info_ps.php?profile_no=3281805mpw20220010&return_page=photograph_missing.php&type=missing&authority=1"
-         }
-         return res
+  if request.method == 'POST':
+    if 'img' not in request.files:
+      return 'there is no file1 in form!'
+    File = request.files['img']
+    format = '.'+File.filename.split('.')[-1]
+    temp = tempfile.NamedTemporaryFile(suffix=format)
+    File.save(temp.name)
+    #load(temp.name)
+    temp.close()
+    res = {
+    "Img": "https://trackthemissingchild.gov.in/trackchild/intra_trackchild/images_missing/3281805mpw20220010.jpg",
+    "Name": "PANCHANU KUMAR RAJAK",
+    "Current_Age": "17 Years 0 Months 6 Days",
+    "Gender": "MALE",
+    "Father_Name": "BHUDEB RAJAK",
+    "Place_of_Missing": "SREEBHUMI",
+    "Date_of_Missing": "10/03/2022",
+    "Profile_link": "https://trackthemissingchild.gov.in/trackchild/missing_dtl.php?		missing_id=47.46.52.45.52.44.49.105.108.115.46.44.46.46.44.44.45.44.102#verticalTab1",
+    "Report_link": "https://trackthemissingchild.gov.in/trackchild/photograph_info_ps.php?profile_no=3281805mpw20220010&return_page=photograph_missing.php&type=missing&authority=1"
+    }
+    return res
 
 
 # init
